0b1f34f2e938f2de95e8196a5f5b5509dac931ab
Oleg <ovchinskiy00@mail.ru>
2022-01-27 16:54:09 +0300

    fix: changed error message

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,11 +7,11 @@ module.exports = function (req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
-      return res.status(401).json({ message: "Не авторизован" });
+      return res.status(401).json({ message: "Пользователь не авторизован" });
     }
     req.user = jwt.verify(token, process.env.ACCESS_TOKEN_KEY);
     next();
   } catch (e) {
-    res.status(401).json({ message: "Не авторизован" });
+    res.status(401).json({ message: "Пользователь не авторизован" });
   }
 };
