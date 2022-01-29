ea21534940a13643a07a6bbc99a7a1684fa5e924
Oleg <ovchinskiy00@mail.ru>
2022-01-29 19:23:57 +0300

    feat: added checkForUpdateMiddleware

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,12 +2,17 @@ const Router = require("express");
 
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
+const checkForUpdateMiddleware = require("../middleware/checkForUpdateMiddleware");
 
 const router = new Router();
 
 router.post("/registration", userController.registration);
 router.post("/login", userController.login);
-router.patch("/updateScore", authMiddleware, userController.updateTotalScore);
+router.patch(
+  "/updateScore",
+  checkForUpdateMiddleware,
+  userController.updateTotalScore
+);
 router.get("/auth", authMiddleware, userController.check);
 
 module.exports = router;
